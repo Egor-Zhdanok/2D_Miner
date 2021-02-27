dcd7b3cc3e1f3d4bd04ecc34f29e67365b26e966
Egor-Zhdanok <zhdanokegor@gmail.com>
2021-02-27 18:59:26 +0300

    Project int commit
    I save all my game files

---
+++ Assets/Scripts/Door_Koridor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door_Koridor : MonoBehaviour {
+	private bool inZone = false;
+	private void OnTriggerEnter2D( Collider2D collision)	{
+		inZone = true;
+		Debug.Log("ОТОШЁЛ ОТ ДВЕРИ");
+	}
+	private void OnTriggerExit2D ( Collider2D collision )	{
+		inZone = false;
+		Debug.Log("И НЕ ПОДХОДИ БОЛЬШЕ К ДВЕРИ");
+	}
+
+	public int scene;
+	void Update() {
+		if (Input.GetKeyDown("space") && inZone)
+        	{
+            	print("space key was pressed");
+            	SceneManager.LoadScene(scene);
+        	}
+    
+      }
+}
---
+++ Assets/Scripts/MySCRIPT.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MySCRIPT: MonoBehaviour 
+{
+    public void PressedStartLvl( int sceneID )    
+{
+        SceneManager.LoadScene(sceneID);
+    	}
+
+    public void PressedExit()    
+{
+Debug.Log("Pressed");
+        Application.Quit();
+    	}
+
+}
